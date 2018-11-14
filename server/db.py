9261c00ab971c86771e8a72be5a709641efe482d
Yidadaa <yidadaa@qq.com>
2018-11-14 23:08:17 +0800

    feat: 完成信息/联系人/发送信息等接口

--- server/db.py
+++ server/db.py
@@ -12,6 +12,19 @@ def get_path_of(filename):
     seperator = '\\'
   return file_path[0:rindex]  + seperator + filename
 
+def array2dict(keys, values):
+  """
+  将扁平化的数组转化为字典
+  """
+  dicts = {}
+  for i in range(len(keys)):
+    if type(keys[i]) is dict:
+      for k in keys[i].keys():
+        dicts[k] = array2dict(keys[i][k], values[i: i + len(keys[i][k])])
+    else:
+      dicts[keys[i]] = values[i]
+  return dicts
+
 class DB:
   def __init__(self):
     path = get_path_of('p2pmessage.db')
@@ -35,5 +48,87 @@ class DB:
       userid = list(userid)
       userid = userid[0][0] if len(userid) > 0 else -1
     except Exception as e:
-      return False, e
+      return False, str(e)
     return True, userid
+
+  def check_user(self, user:dict)->(bool, dict):
+    """
+    检查用户登录
+    """
+    cursor = self.conn.cursor()
+    if 'name' not in user or 'password' not in user:
+      return False, '缺少必要的参数'
+    sql = 'SELECT * FROM USER  WHERE name = "%s" AND password = "%s"'%(user['name'], user['password'])
+
+    keys = ['id', 'name', 'address', 'avatar', 'email', 'password']
+
+    try:
+      res = cursor.execute(sql)
+      self.conn.commit()
+      res = list(res)
+      _user = {}
+      if len(res) > 0:
+        _user = array2dict(keys, res[0])
+      else:
+        raise(Exception('No Such User'))
+      res = _user
+    except Exception as e:
+      return False, str(e)
+    return True, res
+
+  def collect_messages(self, userid:int)->(bool, list):
+    # 获取用户的消息
+    cursor = self.conn.cursor()
+    sql = 'SELECT m.id, m.status, m.content, u.id, u.name, u.avatar, u.address, u.email \
+      FROM MESSAGE AS m INNER JOIN USER as u \
+      ON m.to_userid = %d WHERE m.from_userid = u.id' % (int(userid))
+
+    # print(sql)
+
+    keys = ['id', 'status', 'content', { 'user': ['id', 'name', 'avatar', 'address', 'email'] }]
+
+    try:
+      res = cursor.execute(sql)
+      self.conn.commit()
+      res = list(res)
+      messages = [array2dict(keys, v) for v in res]
+
+    except Exception as e:
+      return False, str(e)
+    return True, messages
+
+  def collect_contacts(self, userid:int)->(bool, list):
+    # 获取用户的联系人
+    cursor = self.conn.cursor()
+    sql = 'SELECT c.id, c.name, c.avatar, c.address, c.email\
+      FROM CONTACTS AS u INNER JOIN USER as c \
+      ON u.id = %d WHERE c.id = u.contact_id' % (int(userid))
+
+    print(sql)
+
+    keys = ['id', 'name', 'avatar', 'address', 'email']
+
+    try:
+      res = cursor.execute(sql)
+      self.conn.commit()
+      res = list(res)
+      contacts = [array2dict(keys, v) for v in res]
+    except Exception as e:
+      return False, str(e)
+
+    return True, contacts
+
+  def add_message(self, from_userid:int, to_userid:int, content:str, ts:int)->(bool, str):
+    cursor = self.conn.cursor()
+
+    sql = 'INSERT INTO MESSAGE (from_userid, to_userid, content, ts)\
+      VALUES (%d, %d, "%s", %d)' % (int(from_userid), int(to_userid), content, int(ts))
+
+    print(sql)
+
+    try:
+      cursor.execute(sql)
+      self.conn.commit()
+    except Exception as e:
+      return False, str(e)
+    return True, ''
--- server/server.py
+++ server/server.py
@@ -1,13 +1,87 @@
-from flask import Flask
+from flask import Flask, request
 from db import DB
+import json
 
 app = Flask(__name__)
 db = DB()
 
-@app.route('/register')
+@app.route('/signin', methods=['POST'])
 def register_user():
-  status, res = db.add_user({ 'name': 'test' })
-  return 'ok' + str(status) + str(res)
+  status, res = db.add_user({
+    'name': request.form['username'],
+    'password': request.form['password']
+  })
+  error = {
+    'success': False
+  }
+  if status is True:
+    return json.dumps({ 'success': status, 'userid': res })
+  else:
+    error['error_msg'] = res
+    return json.dumps(error)
+
+@app.route('/login', methods=['POST'])
+def login():
+  status, res = db.check_user({
+    'name': request.form['username'],
+    'password': request.form['password']
+  })
+  error = {
+    'success': False
+  }
+
+  print(status, res)
+
+  if status is True:
+    return json.dumps({ 'success': status, 'user': res })
+  else:
+    return json.dumps(error)
+
+@app.route('/messages', methods=['post'])
+def get_messages():
+  status, res = db.collect_messages(request.form['userid'])
+  error = {
+    'success': False
+  }
+
+  print(status, res)
+
+  if status is True:
+    return json.dumps({ 'success': status, 'data': res })
+  else:
+    return json.dumps(error)
+
+@app.route('/contacts', methods=['post'])
+def get_contacts():
+  status, res = db.collect_contacts(request.form['userid'])
+  error = {
+    'success': False
+  }
+
+  print(status, res)
+
+  if status is True:
+    return json.dumps({ 'success': status, 'data': res })
+  else:
+    return json.dumps(error)
+
+@app.route('/send', methods=['post'])
+def send_messages():
+  status, res = db.add_message(
+    request.form['from_userid'],
+    request.form['to_userid'],
+    request.form['content'],
+    request.form['ts'])
+  error = {
+    'success': False
+  }
+
+  print(status, res)
+
+  if status is True:
+    return json.dumps({ 'success': status })
+  else:
+    return json.dumps(error)
 
 if __name__ == "__main__":
     app.run(debug=True, port=80, host='0.0.0.0')
